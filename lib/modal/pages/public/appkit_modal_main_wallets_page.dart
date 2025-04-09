02b51ddc20863add7f0c76dc7d14086171735440
Santhosh0617 <santhosh.azotos@gmail.com>
2025-04-09 15:27:01 +0530

    Add Column for the minimum size

--- lib/modal/pages/public/appkit_modal_main_wallets_page.dart
+++ lib/modal/pages/public/appkit_modal_main_wallets_page.dart
@@ -116,85 +116,91 @@ class _AppKitModalMainWalletsPageState
             maxHeight += 30.0;
           }
           final itemsToShow = items.getRange(0, itemsCount);
-          return WalletsList(
-            onTapWallet: (data) {
-              service.selectWallet(data);
-              widgetStack.instance.push(const ConnectWalletPage());
-            },
-            firstItem: Column(
-              children: [
-                EmailLoginInputField(),
-                Visibility(
-                  visible: emailEnabled || socialEnabled,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 4.0,
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              WalletsList(
+                onTapWallet: (data) {
+                  service.selectWallet(data);
+                  widgetStack.instance.push(const ConnectWalletPage());
+                },
+                firstItem: Column(
+                  children: [
+                    EmailLoginInputField(),
+                    Visibility(
+                      visible: emailEnabled || socialEnabled,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 4.0,
+                        ),
+                        child: Column(
+                          children: [
+                            SocialLoginButtonsView(),
+                            // _LoginDivider(),
+                          ],
+                        ),
+                      ),
                     ),
-                    child: Column(
-                      children: [
-                        SocialLoginButtonsView(),
-                        // _LoginDivider(),
-                      ],
-                    ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-            itemList: itemsToShow.toList(),
-            bottomItems: [
-              // Padding(
-              //   padding: const EdgeInsets.symmetric(horizontal: 4.0),
-              //   child: (!modalInstance.featuresConfig.showMainWallets &&
-              //           (emailEnabled || socials.isNotEmpty))
-              //       ? AllWalletsItem(
-              //           title: 'Continue with a wallet',
-              //           titleAlign: TextAlign.center,
-              //           leading: RoundedIcon(
-              //             padding: 10.0,
-              //             assetPath:
-              //                 'lib/modal/assets/icons/regular/wallet.svg',
-              //             assetColor: themeColors.foreground100,
-              //             circleColor: Colors.transparent,
-              //             borderColor: Colors.transparent,
-              //           ),
-              //           onTap: () {
-              //             widgetStack.instance.push(
-              //               const ReownAppKitModalAllWalletsPage(),
-              //               event: ClickAllWalletsEvent(),
-              //             );
-              //           },
-              //         )
-              //       : AllWalletsItem(
-              //           trailing: (items.length <= kShortWalletListCount)
-              //               ? null
-              //               : ValueListenableBuilder<int>(
-              //                   valueListenable:
-              //                       _explorerService.totalListings,
-              //                   builder: (context, value, _) {
-              //                     return WalletItemChip(
-              //                       value: value.lazyCount,
-              //                     );
-              //                   },
-              //                 ),
-              //           onTap: () {
-              //             if (items.length <= kShortWalletListCount) {
-              //               widgetStack.instance.push(
-              //                 const ReownAppKitModalQRCodePage(),
-              //                 event: SelectWalletEvent(
-              //                   name: 'WalletConnect',
-              //                   explorerId: '',
-              //                   platform: AnalyticsPlatform.qrcode,
-              //                 ),
-              //               );
-              //             } else {
-              //               widgetStack.instance.push(
-              //                 const ReownAppKitModalAllWalletsPage(),
-              //                 event: ClickAllWalletsEvent(),
-              //               );
-              //             }
-              //           },
-              //         ),
-              // ),
+                itemList: itemsToShow.toList(),
+                bottomItems: [
+                  // Padding(
+                  //   padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  //   child: (!modalInstance.featuresConfig.showMainWallets &&
+                  //           (emailEnabled || socials.isNotEmpty))
+                  //       ? AllWalletsItem(
+                  //           title: 'Continue with a wallet',
+                  //           titleAlign: TextAlign.center,
+                  //           leading: RoundedIcon(
+                  //             padding: 10.0,
+                  //             assetPath:
+                  //                 'lib/modal/assets/icons/regular/wallet.svg',
+                  //             assetColor: themeColors.foreground100,
+                  //             circleColor: Colors.transparent,
+                  //             borderColor: Colors.transparent,
+                  //           ),
+                  //           onTap: () {
+                  //             widgetStack.instance.push(
+                  //               const ReownAppKitModalAllWalletsPage(),
+                  //               event: ClickAllWalletsEvent(),
+                  //             );
+                  //           },
+                  //         )
+                  //       : AllWalletsItem(
+                  //           trailing: (items.length <= kShortWalletListCount)
+                  //               ? null
+                  //               : ValueListenableBuilder<int>(
+                  //                   valueListenable:
+                  //                       _explorerService.totalListings,
+                  //                   builder: (context, value, _) {
+                  //                     return WalletItemChip(
+                  //                       value: value.lazyCount,
+                  //                     );
+                  //                   },
+                  //                 ),
+                  //           onTap: () {
+                  //             if (items.length <= kShortWalletListCount) {
+                  //               widgetStack.instance.push(
+                  //                 const ReownAppKitModalQRCodePage(),
+                  //                 event: SelectWalletEvent(
+                  //                   name: 'WalletConnect',
+                  //                   explorerId: '',
+                  //                   platform: AnalyticsPlatform.qrcode,
+                  //                 ),
+                  //               );
+                  //             } else {
+                  //               widgetStack.instance.push(
+                  //                 const ReownAppKitModalAllWalletsPage(),
+                  //                 event: ClickAllWalletsEvent(),
+                  //               );
+                  //             }
+                  //           },
+                  //         ),
+                  // ),
+                ],
+              ),
             ],
           );
         },
